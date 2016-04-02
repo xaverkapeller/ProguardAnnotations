19387761f32c6c5c437d66c80490ed963290d481
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-04-02 19:00:58 +0200

    Switched to depending on apt plugin instead of manually configuring annotation processor.

--- proguard-annotations/src/main/groovy/com/github/wrdlbrnft/proguardannotations/ProguardAnnotationsPlugin.groovy
+++ proguard-annotations/src/main/groovy/com/github/wrdlbrnft/proguardannotations/ProguardAnnotationsPlugin.groovy
@@ -12,76 +12,40 @@ class ProguardAnnotationsPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        if(!project.plugins.findPlugin('com.neenbedankt.android-apt')) {
+            throw new ProjectConfigurationException('Android APT Plugin is required for ProguardAnnotations to work. ' +
+                    'You need to apply it before applying proguard-annotations', null)
+        }
+
         def variants = determineVariants(project)
-        def configuration = project.configurations
-                .create('proguardprocessor')
-                .extendsFrom(project.configurations.compile, project.configurations.provided)
+        project.afterEvaluate {
+            project.android[variants].all { variant ->
+                def javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
+                javaCompile.doLast {
+                    println ''
+                    println '######################'
+                    variantOutputDir.listFiles().each {
+                        println it.name
+                    }
+                    println '######################'
+                    println ''
+                }
+            }
+        }
 
         project.dependencies {
             compile 'com.github.wrdlbrnft:proguard-annotations-api:0.1.0.3'
-            proguardprocessor 'com.github.wrdlbrnft:proguard-annotations-processor:0.1.0.3'
-        }
-
-        project.afterEvaluate {
-            project.android[variants].all { variant ->
-                configureVariant(project, variant, configuration)
-            }
+            apt 'com.github.wrdlbrnft:proguard-annotations-processor:0.1.0.3'
         }
     }
 
     private static String determineVariants(Project project) {
-        if (project.plugins.findPlugin("com.android.application") || project.plugins.findPlugin("android") || project.plugins.findPlugin("com.android.test")) {
+        if (project.plugins.findPlugin("com.android.application")) {
             return "applicationVariants";
-        } else if (project.plugins.findPlugin("com.android.library") || project.plugins.findPlugin("android-library")) {
+        } else if (project.plugins.findPlugin("com.android.library")) {
             return "libraryVariants";
         } else {
-            throw new ProjectConfigurationException("The android or android-library plugin must be applied to the project", null)
-        }
-    }
-
-    private static void configureVariant(def project, def variant, def configuration) {
-        if (configuration.empty) {
-            project.logger.info("No progurd dependencies for configuration ${configuration.name}");
-            return;
-        }
-
-        def rootOutputDir = project.file(new File(project.buildDir, "generated/proguard"))
-        def variantOutputDir = new File(rootOutputDir, variant.dirName)
-
-        def javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
-
-        def taskDependency = configuration.buildDependencies
-        if (taskDependency) {
-            javaCompile.dependsOn += taskDependency
-        }
-
-        javaCompile.options.compilerArgs += [
-                '-s', variantOutputDir
-        ]
-
-        javaCompile.doFirst {
-            variantOutputDir.mkdirs()
-        }
-
-        javaCompile.doLast {
-            println ''
-            println '######################'
-            variantOutputDir.listFiles().each {
-                println it.name
-            }
-            println '######################'
-            println ''
-        }
-
-        def dependency = javaCompile.finalizedBy;
-        def dependencies = dependency.getDependencies(javaCompile);
-        for (def dep : dependencies) {
-            if (dep instanceof GroovyCompile) {
-                if (dep.groovyOptions.hasProperty("javaAnnotationProcessing")) {
-                    dep.options.compilerArgs += javaCompile.options.compilerArgs;
-                    dep.groovyOptions.javaAnnotationProcessing = true
-                }
-            }
+            throw new ProjectConfigurationException("The com.android.application or com.android.library plugin must be applied to the project", null)
         }
     }
 }
