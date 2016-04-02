73209150042f5d4bede9bb27a180c5b427a76fa2
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-04-02 19:43:05 +0200

    Plugin now filters the required proguard files.

--- proguard-annotations/src/main/groovy/com/github/wrdlbrnft/proguardannotations/ProguardAnnotationsPlugin.groovy
+++ proguard-annotations/src/main/groovy/com/github/wrdlbrnft/proguardannotations/ProguardAnnotationsPlugin.groovy
@@ -12,7 +12,7 @@ class ProguardAnnotationsPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        if(!project.plugins.findPlugin('com.neenbedankt.android-apt')) {
+        if (!project.plugins.findPlugin('com.neenbedankt.android-apt')) {
             throw new ProjectConfigurationException('Android APT Plugin is required for ProguardAnnotations to work. ' +
                     'You need to apply it before applying proguard-annotations', null)
         }
@@ -26,7 +26,7 @@ class ProguardAnnotationsPlugin implements Plugin<Project> {
                 javaCompile.doLast {
                     println ''
                     println '######################'
-                    variantOutputDir.listFiles().each {
+                    findProguardFiles(variantOutputDir).each {
                         println it.name
                     }
                     println '######################'
@@ -39,6 +39,22 @@ class ProguardAnnotationsPlugin implements Plugin<Project> {
             compile 'com.github.wrdlbrnft:proguard-annotations-api:0.1.0.5'
             apt 'com.github.wrdlbrnft:proguard-annotations-processor:0.1.0.5'
         }
+    }
+
+    private static Set<File> findProguardFiles(File folder) {
+        def proguardFiles = new HashSet();
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                proguardFiles.addAll(findProguardFiles(file))
+                continue
+            }
+
+            def fileName = file.name
+            if(fileName.startsWith('generated_proguard_part_') && fileName.endsWith('.pro')) {
+                proguardFiles.add(file)
+            }
+        }
+        return proguardFiles;
     }
 
     private static String determineVariants(Project project) {
