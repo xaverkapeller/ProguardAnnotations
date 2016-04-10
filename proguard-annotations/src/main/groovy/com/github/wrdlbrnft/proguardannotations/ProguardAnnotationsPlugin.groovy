96c72cb599d169bf29104f26b921fcc53fa28404
Xaver Kapeller <xaver.kapeller@gmail.com>
2016-04-11 01:31:49 +0200

    Removed dependency on apt plugin.

--- proguard-annotations/src/main/groovy/com/github/wrdlbrnft/proguardannotations/ProguardAnnotationsPlugin.groovy
+++ proguard-annotations/src/main/groovy/com/github/wrdlbrnft/proguardannotations/ProguardAnnotationsPlugin.groovy
@@ -11,23 +11,25 @@ class ProguardAnnotationsPlugin implements Plugin<Project> {
 
     private static final String GENERATED_RULE_FILE_NAME = 'generated-proguard-rules.pro'
 
+    private File mGeneratedProguardFile;
+
     @Override
     void apply(Project project) {
+        mGeneratedProguardFile = new File(project.buildDir, GENERATED_RULE_FILE_NAME)
         project.afterEvaluate {
             def variants = determineVariants(project)
-            ensureAptPluginIsApplied(project)
 
             project.android[variants].all { variant ->
                 configureVariant(project, variant)
             }
 
             project.android.buildTypes.each { type ->
-                type.proguardFiles new File(project.buildDir, GENERATED_RULE_FILE_NAME)
+                type.proguardFiles mGeneratedProguardFile
             }
 
             project.dependencies {
                 provided 'com.github.wrdlbrnft:proguard-annotations-api:0.2.0.39'
-                apt 'com.github.wrdlbrnft:proguard-annotations-processor:0.2.0.39'
+                provided 'com.github.wrdlbrnft:proguard-annotations-processor:0.2.0.39'
             }
         }
     }
@@ -42,25 +44,17 @@ class ProguardAnnotationsPlugin implements Plugin<Project> {
         }
     }
 
-    private static void ensureAptPluginIsApplied(Project project) {
-        if (!project.plugins.findPlugin('com.neenbedankt.android-apt')) {
-            throw new ProjectConfigurationException('Android APT Plugin is required for ProguardAnnotations to work.', null)
-        }
-    }
-
     private static void configureVariant(Project project, def variant) {
-        def rootOutputDir = project.file(new File(project.buildDir, "generated/source/apt"))
-        def variantOutputDir = new File(rootOutputDir, variant.dirName)
         def javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
         javaCompile.doLast {
-            def generatedProguardFile = new File(project.buildDir, GENERATED_RULE_FILE_NAME)
-            generatedProguardFile.withWriter { out ->
+            mGeneratedProguardFile.withWriter { out ->
                 out.println '-keepattributes InnerClasses, Signature, Annotations'
-                findProguardFiles(variantOutputDir).each {
-                    def trimmedContent = it.text.trim()
+                findProguardFiles(project.buildDir).each { file ->
+                    def trimmedContent = file.text.trim()
                     if (!trimmedContent.isEmpty()) {
                         out.println trimmedContent
                     }
+                    file.delete();
                 }
             }
         }
