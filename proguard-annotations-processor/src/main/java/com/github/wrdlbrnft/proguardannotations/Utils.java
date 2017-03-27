1abbce75d20f67d344ec2c6eed9155c851fc649e
Xaver Kapeller <xaver.kapeller@gmail.com>
2017-03-27 19:08:55 +0200

    Complex generics and wildcards as well as wildcards based on type parameters should now be handled correctly.

--- proguard-annotations-processor/src/main/java/com/github/wrdlbrnft/proguardannotations/Utils.java
+++ proguard-annotations-processor/src/main/java/com/github/wrdlbrnft/proguardannotations/Utils.java
@@ -5,9 +5,11 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Created by Xaver on 09/04/16.
@@ -33,10 +35,32 @@ public class Utils {
 
     public static String getProguardClassName(ProcessingEnvironment processingEnv, TypeMirror mirror) {
         final TypeKind kind = mirror.getKind();
-        if(kind == TypeKind.TYPEVAR) {
-            return "***";
+        if (kind == TypeKind.TYPEVAR) {
+            return "**";
         }
-        
+
+        if (kind == TypeKind.WILDCARD) {
+            return "**";
+        }
+
+        if (mirror instanceof DeclaredType) {
+            final DeclaredType declaredType = (DeclaredType) mirror;
+
+            final String resolveTypeParameters = declaredType.getTypeArguments().isEmpty()
+                    ? ""
+                    : declaredType.getTypeArguments().stream()
+                    .map(argument -> getProguardClassName(processingEnv, mirror))
+                    .collect(Collectors.joining(", ", "<", ">"));
+
+            final TypeMirror erasedType = processingEnv.getTypeUtils().erasure(declaredType);
+            final Element element = processingEnv.getTypeUtils().asElement(erasedType);
+            if (element instanceof TypeElement) {
+                final TypeElement typeElement = (TypeElement) element;
+                return getProguardClassName(typeElement) + resolveTypeParameters;
+            }
+            return erasedType.toString() + resolveTypeParameters;
+        }
+
         final Element element = processingEnv.getTypeUtils().asElement(mirror);
         if (element instanceof TypeElement) {
             final TypeElement typeElement = (TypeElement) element;
